7eaace3b4d2dc8faeeb8a1a1e4322f2c4364150a
cleudiney <cleudiney@hotmail.com>
2022-03-09 19:51:45 -0300

    pastas atualizadas

--- 25081b1a-cba8-4e7d-945c-8453a64238f5/app-ifood-clone-master/src/AppEntry.js
+++
@@ -1,5 +0,0 @@
-import registerRootComponent from 'expo/build/launch/registerRootComponent';
-
-import Routes from './routes';
-
-registerRootComponent(Routes);
--- clone-ifood-master/clone-ifood-master/App.js
+++
@@ -1,9 +0,0 @@
-import 'react-native-gesture-handler';
-import React from 'react';
-import App from './src/index.js'
-
-export default function Main(){
-  return(
-    <App />
-  )
-}
